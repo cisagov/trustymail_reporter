9ac5d2e47aeda6f4789563e8cdcbda0418f9c12a
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-01-19 08:48:49 -0500

    Changes to make pshtt and trustymail reporters play nice

--- report.sh
+++ report.sh
@@ -15,36 +15,43 @@ do
 done
 echo "Saver finished"
 
-# No longer needed
-redis-cli -h orchestrator_redis_1 del saving_complete
+# Don't delete saving_complete here since pshtt_reporter may be using
+# it too.  We let that container do the delete.  If that container
+# isn't being used, though, then you'll want to uncomment the next
+# line.
+# redis-cli -h orchestrator_redis_1 del saving_complete
 
 echo "Creating reporting folder..."
-mkdir -p $SHARED_DIR/artifacts/reporting
+mkdir -p $SHARED_DIR/artifacts/reporting/trustymail_reports
 
 # Create the Scorecard
 cd $SHARED_DIR/artifacts/reporting/
 $HOME_DIR/scorecard/generate_trustymail_scorecard.py $HOME_DIR/include/agencies.csv -f
-
-mkdir -p $SHARED_DIR/artifacts/reporting/trustymail_reports
 
 # Generate agency reports
 # TODO? Separate cyhy reports from non-cyhy reports
 cd $SHARED_DIR/artifacts/reporting/trustymail_reports
 $HOME_DIR/report/create_all_reports.py
 
+# Again, we let pshtt_reporter do the archiving.  If that container
+# isn't being used, though, then you'll want to uncomment the next
+# block.
+
 # Archive artifacts folder
-echo 'Archiving Results...'
-mkdir -p $SHARED_DIR/archive/
-cd $SHARED_DIR
-TODAY=$(date +'%Y-%m-%d')
-mv artifacts artifacts_$TODAY
-tar -czf $SHARED_DIR/archive/artifacts_$TODAY.tar.gz artifacts_$TODAY/
-
+# echo 'Archiving Results...'
+# mkdir -p $SHARED_DIR/archive/
+# cd $SHARED_DIR
+# TODAY=$(date +'%Y-%m-%d')
+# mv artifacts artifacts_$TODAY
+# tar -czf $SHARED_DIR/archive/artifacts_$TODAY.tar.gz artifacts_$TODAY/
 # Clean up
-echo 'Cleaning up'
-rm -rf artifacts_$TODAY
+# echo 'Cleaning up'
+# rm -rf artifacts_$TODAY
 
-# Let redis know we're done
-# redis-cli -h orchestrator_redis_1 set trustymail_reporting_complete true
+# Let redis know we're done.  We let the pshtt_reporter container do
+# the actual shutdown.  If that container isn't being used, though,
+# then you'll instead want to uncomment the shutdown line below.
+redis-cli -h orchestrator_redis_1 set trustymail_reporting_complete true
+
 # This is the end of the line, so tell redis to shutdown
-redis-cli -h orchestrator_redis_1 shutdown
+# redis-cli -h orchestrator_redis_1 shutdown
