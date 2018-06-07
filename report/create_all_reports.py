01651fbc04d6afeb7a03863ba7ba92eecde46ba4
Jeremy Frasier <jeremy.frasier@beta.dhs.gov>
2018-06-07 14:59:10 -0400

    Adding code to download and preprocess BGP data for pyasn

--- report/create_all_reports.py
+++ report/create_all_reports.py
@@ -3,15 +3,23 @@
 import os
 import csv
 
+import pyasn
+
 HOME_DIR = '/home/reporter'
 SHARED_DATA_DIR = HOME_DIR + '/shared/'
 
 def main():
-    agency_csv = open(SHARED_DATA_DIR + "artifacts/unique-agencies.csv")
+    # Download and preprocess some BGP data for later use by pyasn
+    # inside of generate_trustymail_report.py
+    print('Downloading BGP data for pyasn...')
+    os.system('pyasn_util_download.py --latestv46')
+    print('Preprocessing BGP data for pyasn...')
+    os.system('pyasn_util_convert.py --single rib.*.bz2 ipasn.dat')
 
+    agency_csv = open(SHARED_DATA_DIR + 'artifacts/unique-agencies.csv')
     for row in sorted(csv.reader(agency_csv)):
-        bashCommand = HOME_DIR + "/report/generate_trustymail_report.py " + '"' + row[0] + '"'
+        bashCommand = HOME_DIR + '/report/generate_trustymail_report.py ' + '"' + row[0] + '"'
         os.system(bashCommand)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
